383ba69eab346906e125200b629d4af4f9d2e714
d3j1x <d3j1x@yahoo.com>
2024-08-19 21:26:27 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script></script>
 
-<div class="bg-white pt-14">
+<div class="bg-white pt-14 mx-1 rounded-sm">
 
-<h1 class="pt-1 text-center text-base text-gray-950 z-50">𝕯𝖎𝖌𝖎𝖙𝖆𝖑 𝕮𝖗𝖊𝖆𝖙𝖔𝖗 <span class="animate-pulse text-blue-600">✪</span></h1>
+<h1 class="pt-1 text-center font-semibold text-sm text-black z-50">𝕯𝖎𝖌𝖎𝖙𝖆𝖑 𝕮𝖗𝖊𝖆𝖙𝖔𝖗 <span class="animate-pulse text-blue-600">✪ </span> 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 <span class="animate-pulse text-purple-600">🛠 </span> 𝕮𝖞𝖇𝖊𝖗𝖘𝖊𝖈𝖚𝖗𝖎𝖙𝖞 𝕾𝖕𝖊𝖈𝖎𝖆𝖑𝖎𝖘𝖙 <span class="animate-pulse text-blue-600">🛡 </span></h1>
 
 </div>
 
