fbc91b70797f2b9ae50a369111271aebeebd4d48
d3j1x <d3j1x@yahoo.com>
2024-08-25 23:23:36 +0200

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,14 @@
 <script></script>
 
+<svelte:head>
+  <title>𝐒𝐡𝐢𝐡𝐚𝐛𝐝𝐢𝐧 ✪</title>
+  <meta name="description" content="𝙎𝙝𝙞𝙝𝙖𝙗𝙙𝙞𝙣 𝙋𝙤𝙧𝙩𝙛𝙤𝙡𝙞𝙤.
+  ℂ𝕪𝕓𝕖𝕣𝕤𝕖𝕔𝕦𝕣𝕚𝕥𝕪 𝕊𝕡𝕖𝕔𝕚𝕒𝕝𝕚𝕤𝕥 🔐 | 𝕊𝕧𝕖𝕝𝕥𝕖𝕜𝕚𝕥 𝔻𝕖𝕧𝕖𝕝𝕠𝕡𝕖𝕣 🔥 | 𝕆𝕡𝕖𝕟𝕤𝕠𝕦𝕣𝕔𝕖 💎.
+  "/>
+</svelte:head>
+
 <div class="bg-white pt-14 mx-1 rounded-sm">
-
-<h1 class="pt-1 text-center font-semibold text-xs text-black z-50">𝕯𝖎𝖌𝖎𝖙𝖆𝖑 𝕮𝖗𝖊𝖆𝖙𝖔𝖗 <span class="animate-pulse text-blue-600">✪ </span> 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 <span class="animate-pulse text-purple-600">🛠 </span> 𝕮𝖞𝖇𝖊𝖗𝖘𝖊𝖈𝖚𝖗𝖎𝖙𝖞 𝕾𝖕𝖊𝖈𝖎𝖆𝖑𝖎𝖘𝖙 <span class="animate-pulse text-blue-600">🛡 </span></h1>
-
+    <h1 class="pt-1 text-center font-semibold text-xs text-black z-50">𝕮𝖞𝖇𝖊𝖗𝖘𝖊𝖈𝖚𝖗𝖎𝖙𝖞 <span class="animate-pulse text-blue-600">🛡 </span> 𝕾𝖛𝖊𝖑𝖙𝖊𝖐𝖎𝖙 𝕯𝖊𝖛𝖊𝖑𝖔𝖕𝖊𝖗 <span class="animate-pulse text-purple-600">🛠 </span> 𝕺𝖕𝖊𝖓𝖘𝖔𝖚𝖗𝖈𝖊 <span class="animate-pulse text-blue-600">💎</span></h1>
 </div>
 
 
