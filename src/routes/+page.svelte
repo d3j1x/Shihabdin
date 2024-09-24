9fa15254c145adedcfa510ff8e423b1b2a97639a
d3j1x <d3j1x@yahoo.com>
2024-09-24 12:05:31 +0200

    update home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,47 +20,52 @@ import { GiftBoxSolid } from 'flowbite-svelte-icons';
 
 <main>
 
-<div class="p-6 md:p-10"></div>
-<div>
+<div class="py-10"></div>
+
+<div class="glass-container py-4 mx-6">
+  
   
   <h1 style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
-    class="py-6 my-10 text-3xl md:text-4xl xl:text-5xl text-center font-bold text-primary-200">
-    Best Projects</h1>
+    class="py-2 mt-4 mb-8 text-4xl md:text-5xl xl:text-6xl text-center font-bold text-primary-200">
+    Best Projects
+  </h1>
+
+
+  <div class=" grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-2 m-5 xl:gap-2 xl:mx-20">
+    <div class="col-span-1 mx-auto md:my-20 flex-wrap">
+        <Card class="bg-transparent hover:backdrop-blur-sm shadow-blue-600 border-none backdrop-blur-none">
+            <a href="https://smx.tn" target="_blank" rel="noreferrer">
+              <h5 class="my-2 text-center text-2xl md:text-2xl lg:text-3xl tracking-tight text-blue-600 font-extrabold">
+                SMRT MRKT X
+              </h5>
+            </a>
+            <p class="mb-3 font-bold text-gray-400  lg:text-lg">Join our community to stay ahead in the dynamic world of digital. SMX</p>
+            <a href="https://smx.tn" target="_blank" rel="noreferrer" class="inline-flex items-center text-blue-600 hover:underline font-semibold">
+              Explore now
+              <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+            </a>
+        </Card>
+    </div>
+    <div class="col-span-1 m-auto">
+        <Card class="bg-transparent hover:backdrop-blur-sm shadow-yellow-300 border-none backdrop-blur-none">
+            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer">
+              <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-yellow-300 font-extrabold">
+                EARNMONEY</h5>
+            </a>
+            <p class="mb-3 font-bold text-gray-400 dark:text-gray-400 lg:text-lg">Discover the secrets to earning passive income through cryptocurrency.
+            </p>
+            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer" class="inline-flex items-center text-yellow-300 hover:underline font-semibold">
+              Explore now
+              <GiftBoxSolid class="w-4 h-4 ms-2.5" />
+            </a>
+        </Card>
+    </div>
+    
+
+  </div>
+
 </div>
 
-<div class="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-2 m-5 xl:gap-2 xl:mx-20">
-  <div class="col-span-1 mx-auto md:my-20 flex-wrap">
-      <Card class="bg-transparent hover:backdrop-blur-sm shadow-blue-600 border-none backdrop-blur-none">
-          <a href="https://smx.tn" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-center text-2xl md:text-2xl lg:text-3xl tracking-tight text-blue-600 font-extrabold">
-              SMRT MRKT X
-            </h5>
-          </a>
-          <p class="mb-3 font-bold text-gray-400  lg:text-lg ">Join our community to stay ahead in the dynamic world of digital. SMX</p>
-          <a href="https://smx.tn" target="_blank" rel="noreferrer" class="inline-flex items-center text-blue-600 hover:underline font-semibold">
-            Explore now
-            <GiftBoxSolid class="w-4 h-4 ms-2.5" />
-          </a>
-      </Card>
-  </div>
-  <div class="col-span-1 m-auto">
-      <Card class="bg-transparent hover:backdrop-blur-sm shadow-yellow-300 border-none backdrop-blur-none">
-          <a href="https://earnmoney.tn" target="_blank" rel="noreferrer">
-            <h5 class="my-2 text-2xl md:text-2xl lg:text-3xl  tracking-tight text-center text-yellow-300 font-extrabold">
-              EARNMONEY</h5>
-          </a>
-          <p class="mb-3 font-bold text-gray-400 dark:text-gray-400 lg:text-lg">Discover the secrets to earning passive income through cryptocurrency.
-          </p>
-          <a href="https://earnmoney.tn" target="_blank" rel="noreferrer" class="inline-flex items-center text-yellow-300 hover:underline font-semibold">
-            Explore now
-            <GiftBoxSolid class="w-4 h-4 ms-2.5" />
-          </a>
-      </Card>
-  </div>
-  
-
-
-</div>
 
 </main>
 
