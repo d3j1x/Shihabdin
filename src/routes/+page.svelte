b8b539d69a697a86a1e6bab9aa54badabe758b93
d3j1x <d3j1x@yahoo.com>
2024-09-26 17:51:07 +0200

    Update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -36,7 +36,7 @@ onMount(()=>{
 py-2 mx-auto">
   <h1 style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
   class="hero__heading opacity-0 mb-8 mt-4 py-2 md:py-4 lg:py-6 md:mt-8 
-  text-4xl md:text-6xl xl:text-7xl text-center  
+  text-5xl md:text-6xl lg:text-7xl text-center  
     bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text text-transparent tracking-wide">
     Best 
     <a class=" hover:bg-gradient-to-t hover:from-yellow-100 hover:to-yellow-500
@@ -46,22 +46,22 @@ py-2 mx-auto">
 
 
   <div class="hero__body opacity-0 grid grid-cols-1 md:grid-cols-2 
-    gap-10 xl:gap-20 lg:gap-32 m-5 xl:mx-20">
+    gap-10 xl:gap-20 m-5 xl:mx-40">
     
     
     
     
     <div class="myCustombg1 rounded-md col-span-1 mx-auto md:my-16  
-    bg-primary-950 transition-transform duration-300 hover:scale-105 hover:bg-black/50">
+    bg-black transition-transform duration-300 hover:scale-105 hover:bg-transparent">
         <Card class="bg-transparent shadow-sm   shadow-yellow-300 border-none 
         backdrop-blur-none">
             <a href="https://smx.tn" target="_blank" rel="noreferrer">
               <h5 class="my-4 text-center text-2xl md:text-3xl lg:text-4xl
-               text-gray-300 font-extrabold goldHover">
+               text-gray-100 font-extrabold goldHover">
                 SMRT MRKT X
               </h5>
             </a>
-            <p class="my-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
+            <p class="mb-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
               Join our community to stay ahead in the dynamic world.</p>
             <a href="https://smx.tn" target="_blank" rel="noreferrer" 
             class="inline-flex items-center text-gray-200 font-semibold goldHover">
@@ -75,10 +75,10 @@ py-2 mx-auto">
        <Card class="bg-transparent shadow-sm shadow-yellow-300 border-none backdrop-blur-none">
            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer">
              <h5 class="my-4 text-2xl md:text-3xl lg:text-4xl
-             text-center text-gray-300 font-extrabold goldHover">
+             text-center text-gray-200 font-extrabold goldHover">
                EARNMONEY</h5>
            </a>
-           <p class="my-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
+           <p class="mb-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
              Discover the secrets of earning online passive income.
            </p>
            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer" 
@@ -124,6 +124,15 @@ py-2 mx-auto">
 
 
  .goldHover:hover {
+  
+  background: linear-gradient(to bottom, #fefcbf, #f59e0b); /* Gradient from yellow-100 (#fefcbf) to yellow-500 (#f59e0b) */
+  -webkit-background-clip: text; /* Clipping the background to the text */
+  background-clip: text; /* For other browsers */
+  color: transparent; /* Makes the text itself transparent */
+  font-style: normal; /* Ensures the text is not italic */
+ }
+
+ .goldBg {
   
   background: linear-gradient(to bottom, #fefcbf, #f59e0b); /* Gradient from yellow-100 (#fefcbf) to yellow-500 (#f59e0b) */
   -webkit-background-clip: text; /* Clipping the background to the text */
