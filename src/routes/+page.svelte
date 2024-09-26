bda1ded426c24191f311d3653ae651e83bc7b41a
d3j1x <d3j1x@yahoo.com>
2024-09-26 17:15:57 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -32,21 +32,28 @@ onMount(()=>{
 <section>
 <div class=""></div>
 
-<div class="relative border-y bg-gray-950/50 backdrop-blur-md border-yellow-300/30  py-2 mx-auto">
-  <h1 class="hero__heading opacity-0  tracking-tighter mb-8 mt-4 py-2 md:py-4 lg:py-6 md:mt-8 
-  text-4xl md:text-6xl xl:text-7xl text-center font-extrabold 
-    bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text text-transparent">
-    ~Best <a class=" hover:bg-gradient-to-t hover:from-yellow-100 hover:to-yellow-500
+<div class="relative border-y bg-gray-950/50  backdrop-blur-md border-yellow-300/30  
+py-2 mx-auto">
+  <h1 style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
+  class="hero__heading opacity-0 mb-8 mt-4 py-2 md:py-4 lg:py-6 md:mt-8 
+  text-4xl md:text-6xl xl:text-7xl text-center  
+    bg-gradient-to-b from-yellow-100 to-yellow-500 bg-clip-text text-transparent tracking-wide">
+    Best 
+    <a class=" hover:bg-gradient-to-t hover:from-yellow-100 hover:to-yellow-500
     hover:bg-clip-text hover:text-transparent" 
-    href="/projects">Projects</a>~
+    href="/projects">Projects</a>
   </h1>
 
 
   <div class="hero__body opacity-0 grid grid-cols-1 md:grid-cols-2 
     gap-10 xl:gap-20 lg:gap-32 m-5 xl:mx-20">
+    
+    
+    
+    
     <div class="myCustombg1 rounded-md col-span-1 mx-auto md:my-16  
-    bg-black transition-transform duration-300 hover:scale-105">
-        <Card class="bg-transparent shadow-sm  shadow-yellow-300 border-none 
+    bg-primary-950 transition-transform duration-300 hover:scale-105 hover:bg-black/50">
+        <Card class="bg-transparent shadow-sm   shadow-yellow-300 border-none 
         backdrop-blur-none">
             <a href="https://smx.tn" target="_blank" rel="noreferrer">
               <h5 class="my-4 text-center text-2xl md:text-3xl lg:text-4xl
@@ -63,25 +70,30 @@ onMount(()=>{
         </Card>
     </div>
 
-    <div class=" myCustombg2 rounded-md col-span-1 mx-auto md:my-16  bg-black transition-transform duration-300 hover:scale-105">
-        <Card class="bg-transparent shadow-sm shadow-yellow-300 border-none backdrop-blur-none">
-            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer">
-              <h5 class="my-4 text-2xl md:text-3xl lg:text-4xl
-              text-center text-gray-300 font-extrabold goldHover">
-                EARNMONEY</h5>
-            </a>
-            <p class="my-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
-              Discover the secrets of earning online passive income.
-            </p>
-            <a href="https://earnmoney.tn" target="_blank" rel="noreferrer" 
-            class="inline-flex items-center text-gray-200 font-semibold goldHover">
-              Explore now
-            </a>
-        </Card>
-    </div>
+    <div class="bg-primary-950 myCustombg2 rounded-md col-span-1 mx-auto md:my-16  
+    transition-transform duration-300 hover:scale-105 hover:bg-black/50">
+       <Card class="bg-transparent shadow-sm shadow-yellow-300 border-none backdrop-blur-none">
+           <a href="https://earnmoney.tn" target="_blank" rel="noreferrer">
+             <h5 class="my-4 text-2xl md:text-3xl lg:text-4xl
+             text-center text-gray-300 font-extrabold goldHover">
+               EARNMONEY</h5>
+           </a>
+           <p class="my-2 px-4 font-bold text-gray-400 text-lg lg:text-xl lg:my-4 text-center goldHover">
+             Discover the secrets of earning online passive income.
+           </p>
+           <a href="https://earnmoney.tn" target="_blank" rel="noreferrer" 
+           class="inline-flex items-center text-gray-200 font-semibold goldHover">
+             Explore now
+           </a>
+       </Card>
+   </div>
+
+
   </div>
 </div>
 </section>
+
+
 
 
 
