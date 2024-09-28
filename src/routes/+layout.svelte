adc80771f09dc1ac3bd0b90ed92d5d580260e5c9
d3j1x <d3j1x@yahoo.com>
2024-09-28 18:08:17 +0200

    bg Update style

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,19 @@
 <script>
-    import "../app.css";
-    import shihabdinlogo from "$lib/img/shihabdin.png";
-    import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, CloseButton } from 'flowbite-svelte';
+  import "../app.css";
+  import shihabdinlogo from "$lib/img/shihabdin.png";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    CloseButton,
+  } from "flowbite-svelte";
 
-      // Check screen size
-    function checkScreenSize() {
-      return window.innerWidth <= 768; // Adjust the value as needed
-    }
+  // Check screen size
+  function checkScreenSize() {
+    return window.innerWidth <= 768; // Adjust the value as needed
+  }
 
   import { onMount } from "svelte";
   let lastScrollTop = 0;
@@ -14,12 +21,13 @@
 
   onMount(() => {
     const handleScroll = () => {
-      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
 
       if (scrollTop > 50 && scrollTop > lastScrollTop) {
         navbarVisible = false; // Hide navbar when scrolled down more than 50px
       } else if (scrollTop < lastScrollTop || scrollTop <= 50) {
-        navbarVisible = true;  // Show navbar when scrolling up or at the top
+        navbarVisible = true; // Show navbar when scrolling up or at the top
       }
 
       lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // Prevent negative scrolling
@@ -31,51 +39,77 @@
       window.removeEventListener("scroll", handleScroll);
     };
   });
-
 </script>
 
-
-
 <div class:hidden={!navbarVisible}>
-
-
-<Navbar let:toggle dir="ltr" style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
-class="fixed shadow-sm shadow-black bg-transparent backdrop-blur-sm z-50 h-16 -mt-1">
+  <Navbar
+    let:toggle
+    dir="ltr"
+    style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;"
+    class="fixed shadow-sm shadow-black bg-transparent backdrop-blur-sm z-50 h-16 -mt-1"
+  >
     <NavBrand href="/">
-      <img src={shihabdinlogo} class="rounded-full me-3 h-14 md:h-16 z-50" alt="Shihabdin Logo" />
-      <span style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
-      class="self-center whitespace-nowrap text-3xl text-primary-50">Shihabdin
+      <img
+        src={shihabdinlogo}
+        class="rounded-full me-3 h-14 md:h-16 z-50"
+        alt="Shihabdin Logo"
+      />
+      <span
+        style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;"
+        class="self-center whitespace-nowrap text-3xl text-primary-50"
+        >Shihabdin
       </span>
     </NavBrand>
-    <NavHamburger menuClass="text-white hover:text-black"  />
+    <NavHamburger menuClass="text-white hover:text-black" />
     <NavUl class="-mt-6 z-40 md:-mt-0">
-      <NavLi href="/" on:click={() => { if (checkScreenSize()) toggle(); }} class="text-black  md:text-white md:text-xl">Home</NavLi>
-      <NavLi href="/projects" on:click={() => { if (checkScreenSize()) toggle(); }} class="text-black  md:text-white md:text-xl">Projects</NavLi>
-      <NavLi href="/services" on:click={() => { if (checkScreenSize()) toggle(); }} class="text-black  md:text-white md:text-xl">Services</NavLi>
-      <NavLi href="/contact" on:click={() => { if (checkScreenSize()) toggle(); }} class="text-black  md:text-white md:text-xl">Contact</NavLi>
+      <NavLi
+        href="/"
+        on:click={() => {
+          if (checkScreenSize()) toggle();
+        }}
+        class="text-black  md:text-white md:text-xl">Home</NavLi
+      >
+      <NavLi
+        href="/projects"
+        on:click={() => {
+          if (checkScreenSize()) toggle();
+        }}
+        class="text-black  md:text-white md:text-xl">Projects</NavLi
+      >
+      <NavLi
+        href="/services"
+        on:click={() => {
+          if (checkScreenSize()) toggle();
+        }}
+        class="text-black  md:text-white md:text-xl">Services</NavLi
+      >
+      <NavLi
+        href="/contact"
+        on:click={() => {
+          if (checkScreenSize()) toggle();
+        }}
+        class="text-black  md:text-white md:text-xl">Contact</NavLi
+      >
       <CloseButton class="md:hidden" on:click={toggle} />
     </NavUl>
   </Navbar>
-
-</div>
-<div class="myCustombg0">
-
 </div>
 
+<div class="myCustombg0 bg-cover"></div>
 
 <slot></slot>
 
-<h1 style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;" 
-class="fixed bottom-0 w-full justify-center content-center items-center self-center text-center m-auto text-white">
+<h1
+  style="font-family:'Protest Guerrilla', system-ui;font-weight: 400;font-style: normal;"
+  class="fixed bottom-0 w-full justify-center content-center items-center self-center text-center m-auto text-white"
+>
   © 2024 Shihabdin
 </h1>
 
-
-
 <style>
-    .myCustombg0 {
-    background-image: url("../lib/img/cover.jpg"); 
+  .myCustombg0 {
+    background-image: url("../lib/img/cover.jpg");
     height: 10vh;
-
+    background-repeat: no-repeat;
   }
 </style>
